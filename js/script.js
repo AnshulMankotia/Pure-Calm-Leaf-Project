9b3e7a1a939429e28a786c47559a873a4b145bfb
Anshul Mankotia <anshulmankotia1997@gmail.com>
2023-07-04 18:20:38 +0530

    make header responsive for both mobile and tablet

---
+++ js/script.js
@@ -0,0 +1,31 @@
+const hamburgerMenuBtn = document.querySelector("#hamburger-menu");
+const accordion = document.querySelector("#main-accordion");
+const overlay = document.querySelector(".overlay");
+const offCanvasCloseArrow = document.querySelector("#backArrow");
+
+hamburgerMenuBtn.addEventListener("click", function () {
+    accordion.classList.toggle("open");
+    overlay.classList.toggle("open");
+});
+overlay.addEventListener("click", function() {
+    accordion.classList.remove("open");
+    overlay.classList.remove("open");
+  });
+  offCanvasCloseArrow.addEventListener("click", function() {
+    accordion.classList.remove("open");
+    overlay.classList.remove("open");
+  });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    var accordionHeaders = document.querySelectorAll(".accordion-header");
+
+    accordionHeaders.forEach(function (header) {
+        header.addEventListener("click", function () {
+            var accordionItem = this.parentElement;
+            var accordionContent = accordionItem.querySelector(".accordion-content");
+            accordionContent.style.display =
+                accordionContent.style.display === "block" ? "none" : "block";
+        });
+    });
+});
